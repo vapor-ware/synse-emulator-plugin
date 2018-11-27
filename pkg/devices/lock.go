baf9f699ca91d64fdf59f99c70bc3795eb7f0bd4
Hoanh An <hoanhan@bennington.edu>
2018-11-26 23:00:43 -0500

    Fix pulseUnlock action

--- pkg/devices/lock.go
+++ pkg/devices/lock.go
@@ -2,18 +2,28 @@ package devices
 
 import (
 	"fmt"
+	"sync"
+	"time"
 
 	"github.com/vapor-ware/synse-sdk/sdk"
 )
 
 const (
-	stateLock        = "lock"
-	stateUnlock      = "unlock"
-	statePulseUnlock = "pulseUnlock"
+	actionLock        = "lock"
+	actionUnlock      = "unlock"
+	actionPulseUnlock = "pulseUnlock"
 )
 
-// lstate holds the lock state.
-var lstate string
+var (
+	// lockState holds the lock state.
+	lockState string
+
+	// mux provides mutual exclusion for reading/writing to lock state.
+	mux sync.Mutex
+
+	// pulseInterval is the time duration for pulseUnlock, in seconds.
+	pulseInterval time.Duration = 5 * time.Second
+)
 
 // Lock is the handler for the emulated Lock device(s).
 var Lock = sdk.DeviceHandler{
@@ -23,13 +33,17 @@ var Lock = sdk.DeviceHandler{
 }
 
 // lockRead is the read handler for the emulated Lock device(s). It
-// returns the state values for the device.
+// returns the state values for the device. If no state has previously
+// been set, this will set the state to 'lock'
 func lockRead(device *sdk.Device) ([]*sdk.Reading, error) {
-	if lstate == "" {
-		lstate = stateLock
+	mux.Lock()
+	defer mux.Unlock()
+
+	if lockState == "" {
+		lockState = actionLock
 	}
 
-	stateReading, err := device.GetOutput("lock.state").MakeReading(lstate)
+	stateReading, err := device.GetOutput("lock.state").MakeReading(lockState)
 	if err != nil {
 		return nil, err
 	}
@@ -42,26 +56,28 @@ func lockRead(device *sdk.Device) ([]*sdk.Reading, error) {
 // lockWrite is the write handler for the emulated Lock device(s). It
 // sets the state values for the device.
 func lockWrite(_ *sdk.Device, data *sdk.WriteData) error {
-	action := data.Action
-	raw := data.Data
+	mux.Lock()
+	defer mux.Unlock()
 
-	// We always expect the action to come with raw data, so if it
-	// doesn't exist, error.
-	if len(raw) == 0 {
-		return fmt.Errorf("no values specified for 'data', but required")
+	switch action := data.Action; action {
+	case actionLock:
+		lockState = actionLock
+	case actionUnlock:
+		lockState = actionUnlock
+	case actionPulseUnlock:
+		lockState = actionUnlock
+
+		go func() {
+			time.Sleep(pulseInterval)
+
+			mux.Lock()
+			defer mux.Unlock()
+
+			lockState = actionLock
+		}()
+	default:
+		return fmt.Errorf("unsupported command for state action: %v", action)
 	}
 
-	if action == "state" {
-		cmd := string(raw)
-		if cmd == stateLock {
-			lstate = stateLock
-		} else if cmd == stateUnlock {
-			lstate = stateUnlock
-		} else if cmd == statePulseUnlock {
-			lstate = statePulseUnlock
-		} else {
-			return fmt.Errorf("unsupported command for state action: %v", cmd)
-		}
-	}
 	return nil
 }
