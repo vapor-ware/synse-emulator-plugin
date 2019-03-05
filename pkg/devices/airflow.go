2a846d520b4a9ef66e9bf691888bf8467923bafa
Erick Daniszewski <edaniszewski@gmail.com>
2019-03-05 16:38:10 -0500

    allow some read-only devices to have modifiable read range via a writ… (#38)
    * allow some read-only devices to have modifiable read range via a write command
    
    * linting

--- pkg/devices/airflow.go
+++ pkg/devices/airflow.go
@@ -1,20 +1,46 @@
 package devices
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/vapor-ware/synse-emulator-plugin/pkg/utils"
 	"github.com/vapor-ware/synse-sdk/sdk"
 )
 
 // Airflow is the handler for the emulated airflow device(s).
 var Airflow = sdk.DeviceHandler{
-	Name: "airflow",
-	Read: airflowRead,
+	Name:  "airflow",
+	Read:  airflowRead,
+	Write: airflowWrite,
 }
 
 // airflowRead is the read handler for the emulated airflow device(s). It
 // returns random values between -100 and 100.
 func airflowRead(device *sdk.Device) ([]*sdk.Reading, error) {
-	airflow, err := device.GetOutput("airflow").MakeReading(utils.RandIntInRange(-100, 100))
+	// Default reading ranges
+	var min = -100
+	var max = 100
+
+	dState, ok := deviceState[device.ID()]
+	if ok {
+		if _, ok := dState[MIN]; ok {
+			min = dState[MIN].(int)
+		}
+
+		if _, ok := dState[MAX]; ok {
+			max = dState[MAX].(int)
+		}
+	}
+
+	// In the event that we change the min value before the max value to
+	// something greater than what the max value was, make the max value
+	// bigger so we dont panic when generating a random val.
+	if min > max {
+		max = min + 1
+	}
+
+	airflow, err := device.GetOutput("airflow").MakeReading(utils.RandIntInRange(min, max))
 	if err != nil {
 		return nil, err
 	}
@@ -22,4 +48,49 @@ func airflowRead(device *sdk.Device) ([]*sdk.Reading, error) {
 	return []*sdk.Reading{
 		airflow,
 	}, nil
+}
+
+// airflowWrite is the write handler for the emulated airflow device(s).
+// Typically, airflow devices are not writable, but since this is an emulator
+// and we may want to change the returned value(s) of a device at runtime, we can
+// reset the min and max values.
+func airflowWrite(device *sdk.Device, data *sdk.WriteData) error {
+	action := data.Action
+	raw := data.Data
+
+	if len(raw) == 0 {
+		return fmt.Errorf("no values specified for 'data', but required")
+	}
+
+	if action == MIN {
+		// This could get dicey, but since `raw` is bytes and synse server basically just
+		// encodes it as a string, the int value we expect here is actually the bytes for
+		// the string representation of the int...
+		min, err := strconv.Atoi(string(raw))
+		if err != nil {
+			return err
+		}
+		dataMap, ok := deviceState[device.ID()]
+		if !ok {
+			deviceState[device.ID()] = map[string]interface{}{MIN: min}
+		} else {
+			dataMap[MIN] = min
+		}
+
+	} else if action == MAX {
+		// This could get dicey, but since `raw` is bytes and synse server basically just
+		// encodes it as a string, the int value we expect here is actually the bytes for
+		// the string representation of the int...
+		max, err := strconv.Atoi(string(raw))
+		if err != nil {
+			return err
+		}
+		dataMap, ok := deviceState[device.ID()]
+		if !ok {
+			deviceState[device.ID()] = map[string]interface{}{MAX: max}
+		} else {
+			dataMap[MAX] = max
+		}
+	}
+	return nil
 }
--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -5,9 +5,33 @@ import (
 	"time"
 )
 
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
 // RandIntInRange creates a new random integer in the specified range.
-func RandIntInRange(min, max int32) int32 {
-	s1 := rand.NewSource(time.Now().UnixNano())
-	r1 := rand.New(s1)
-	return r1.Int31n(max-min) + min
+func RandIntInRange(min, max int) int {
+	return int(rand.Int63n(int64(max-min))) + min
+}
+
+// Min returns the minimum of the two ints.
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+// PadOrTrim pads or trims a byte array to the specified size.
+func PadOrTrim(bb []byte, size int) []byte {
+	l := len(bb)
+	if l == size {
+		return bb
+	}
+	if l > size {
+		return bb[l-size:]
+	}
+	tmp := make([]byte, size)
+	copy(tmp[size-l:], bb)
+	return tmp
 }
