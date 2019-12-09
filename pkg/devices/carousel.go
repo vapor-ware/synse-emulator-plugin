fb9ad7f9db83d5b065d4cac687532fd58f6cb63c
Erick Daniszewski <edaniszewski@gmail.com>
2019-12-09 14:48:03 -0500

    port emulated carousel for v3 emulator

--- pkg/devices/carousel.go
+++ pkg/devices/carousel.go
@@ -5,18 +5,21 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/vapor-ware/synse-emulator-plugin/pkg/outputs"
+	"github.com/vapor-ware/synse-emulator-plugin/pkg/utils"
 	"github.com/vapor-ware/synse-sdk/sdk"
+	"github.com/vapor-ware/synse-sdk/sdk/output"
 )
 
 const (
 	statusStopped  = "stopped"
 	statusRotating = "rotating"
 
-	stateReady       = "ok"
+	stateReady       = "ready"
 	stateUnavailable = "unavailable"
 )
 
-// Carousel is the handler for emulated carousel device(s).
+// Carousel is the handler for emulator carousel device(s).
 var Carousel = sdk.DeviceHandler{
 	Name:  "carousel",
 	Read:  carouselRead,
@@ -25,94 +28,51 @@ var Carousel = sdk.DeviceHandler{
 
 // carouselRead is the read handler for emulated carousel device(s). It
 // returns the state, status, and position values for the device.
-func carouselRead(device *sdk.Device) ([]*sdk.Reading, error) {
+func carouselRead(device *sdk.Device) ([]*output.Reading, error) {
+	emitter := utils.GetEmitter(device.GetID())
 
-	// Set default reading values - these will be used if they are not cached.
-	state := stateReady
-	status := statusStopped
-	position := 1
-
-	dState, ok := deviceState[device.GUID()]
-	if ok {
-		if _, ok := dState["state"]; ok {
-			state = dState["state"].(string)
-		}
-
-		if _, ok := dState["status"]; ok {
-			status = dState["status"].(string)
-		}
-
-		if _, ok := dState["position"]; ok {
-			position = dState["position"].(int)
-		}
-
-	}
-
-	stateReading, err := device.GetOutput("state").MakeReading(state)
-	if err != nil {
-		return nil, err
-	}
-
-	statusReading, err := device.GetOutput("status").MakeReading(status)
-	if err != nil {
-		return nil, err
-	}
-
-	positionReading, err := device.GetOutput("position").MakeReading(position)
-	if err != nil {
-		return nil, err
-	}
-
-	return []*sdk.Reading{
-		stateReading,
-		statusReading,
-		positionReading,
+	val := emitter.Next().(map[string]interface{})
+	return []*output.Reading{
+		output.State.MakeReading(val["state"]),
+		output.Status.MakeReading(val["status"]),
+		outputs.Position.MakeReading(val["position"]),
 	}, nil
 }
 
 // carouselWrite is the write handler for emulated carousel device(s). It
 // sets the position value for the device.
 func carouselWrite(device *sdk.Device, data *sdk.WriteData) error {
-	action := data.Action
-	raw := data.Data
-
-	// We always expect the action to come with raw data, so if it
-	// doesn't exist, error.
-	if len(raw) == 0 {
+	if len(data.Data) == 0 {
 		return fmt.Errorf("no values specified for 'data', but required")
 	}
 
-	if action == "position" {
-		position, err := strconv.Atoi(string(raw))
+	emitter := utils.GetEmitter(device.GetID())
+	current := emitter.Next().(map[string]interface{})
+
+	switch data.Action {
+	case "position":
+		pos, err := strconv.Atoi(string(data.Data))
 		if err != nil {
 			return err
 		}
 
-		dState, ok := deviceState[device.GUID()]
-		if !ok {
-			deviceState[device.GUID()] = map[string]interface{}{"state": stateUnavailable}
-			deviceState[device.GUID()] = map[string]interface{}{"status": statusRotating}
-		} else {
-			dState["state"] = stateUnavailable
-			dState["status"] = statusRotating
-		}
+		// Set the state to designate that the carousel is rotating.
+		current["state"] = stateUnavailable
+		current["status"] = statusRotating
 
+		// After a short while, update the state so the carousel is done
+		// rotating and in the new position.
 		go func() {
-			// Sleep for a short time before updating the position and changing the
-			// state and status back.
 			time.Sleep(5 * time.Second)
 
-			dState, ok := deviceState[device.GUID()]
-			if !ok {
-				deviceState[device.GUID()] = map[string]interface{}{"state": stateReady}
-				deviceState[device.GUID()] = map[string]interface{}{"status": statusStopped}
-				deviceState[device.GUID()] = map[string]interface{}{"position": position}
-			} else {
-				dState["state"] = stateReady
-				dState["status"] = statusStopped
-				dState["position"] = position
-			}
+			current["state"] = stateReady
+			current["status"] = statusStopped
+			current["position"] = pos
+			emitter.Set(current)
 		}()
+
+	default:
+		return fmt.Errorf("unsupported write action: %v", data.Action)
 	}
 	return nil
 }
